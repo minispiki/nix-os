f36e435c186d314d7d650702868bb7700b2f39e1
minispiki <140969853+minispiki@users.noreply.github.com>
2025-12-06 18:29:10 +0000

    fix perms (for real)

--- hosts/hamburb/system/perms.nix
+++ hosts/hamburb/system/perms.nix
@@ -1,21 +1,20 @@
 {config, lib, pkgs, ...}:
 let
   path = "/etc/nixos";
+  app = "/run/current-system/sw/bin";
 in
 {
   # Fix dir perms
   systemd.services.set-perms = {
     description = "Make /etc/nixos read+write to nix group";
     after = ["network.target"];
-    serviceConfig.ExecStart = ''
-      export PATH=/run/current-system/sw/bin/:$PATH
-      chmod -R 775 ${path}
-      chown -R root:nix ${path}
-    '';
     serviceConfig = {
+      ExecStart = ''
+      ${app}/chmod -R 775 ${path}
+      ${app}/chown -R root:nix ${path}
+    '';
       Type = "oneshot";
       RemainAfterExit = true;
-      Environment = "PATH=/run/current-system/sw/bin/:$PATH";
     };
     wantedBy = ["multi-user.target"];
   };
